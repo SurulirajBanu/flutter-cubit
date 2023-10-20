e9e946a28b5ab2e6614c7701e88242c78a35bd4c
SurulirajBanu <bsuruliraj@routeware.com>
2023-10-20 17:18:11 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Implemented with the help of tutorial: https://www.freecodecamp.org/news/build-a-restful-api-using-node-express-and-mongodb/
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+app.use(express.json());
+
+const routes = require('./routes/routes');
+app.use('/api', routes);
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+});
